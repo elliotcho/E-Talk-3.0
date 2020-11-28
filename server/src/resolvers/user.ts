5aaa36d86ebc2a353584d72ea000c00fa7140cd1
Yong <ycho97@uwo.ca>
2020-11-27 22:51:37 -0500

    next.js setup with typescript

--- server/src/resolvers/user.ts
+++ server/src/resolvers/user.ts
@@ -117,7 +117,7 @@ export class UserResolver{
         }
 
         req.session.uid = user.id;
-
+ 
         return { user };
     }
 }
---
+++ web/src/pages/index.tsx
@@ -0,0 +1,7 @@
+const Index = () => (
+    <div>
+      HELLO
+    </div>
+)
+
+export default Index
